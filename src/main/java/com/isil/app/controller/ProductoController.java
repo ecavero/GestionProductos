110f2930a6d49842be89545cce6260183dbb7c97
Eduardo Cavero <ecavero@gmail.com>
2025-10-29 12:27:00 -0500

    Crear el Producto Controller

---
+++ src/main/java/com/isil/app/controller/ProductoController.java
@@ -0,0 +1,24 @@
+package com.isil.app.controller;
+
+import com.isil.app.model.Producto;
+import com.isil.app.respository.ProductoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ProductoController {
+
+   @Autowired
+   ProductoRepository productoRepository;
+   @GetMapping("/productos")
+   String listarProductos(Model model) {
+     List<Producto> productos = productoRepository.findAll();
+     model.addAttribute("productos", productos);
+     return "index";
+   }
+}
