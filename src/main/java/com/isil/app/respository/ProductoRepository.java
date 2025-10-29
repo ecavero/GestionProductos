2e317a5d21b9a331e4c55669fc947da15ff59a50
Eduardo Cavero <ecavero@gmail.com>
2025-10-29 12:26:37 -0500

    Crear el Producto Repository

---
+++ src/main/java/com/isil/app/respository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.isil.app.respository;
+
+import com.isil.app.model.Producto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
