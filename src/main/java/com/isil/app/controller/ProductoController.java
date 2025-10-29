08afcabff6fa04bd65d3a00bd7c2ea46be63ff38
Eduardo Cavero <ecavero@gmail.com>
2025-10-29 15:03:39 -0500

    Agregar métodos para editar y actualizar producto.

--- src/main/java/com/isil/app/controller/ProductoController.java
+++ src/main/java/com/isil/app/controller/ProductoController.java
@@ -2,11 +2,15 @@ package com.isil.app.controller;
 
 import com.isil.app.model.Producto;
 import com.isil.app.respository.ProductoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +37,20 @@ public class ProductoController {
 
    @PostMapping
    String nuevoProducto(Model model, Producto producto) {
+      productoRepository.save(producto);
+      return "redirect:/productos";
+   }
+
+   @GetMapping("/editar/{id}")
+   String editarProducto(Model model, @PathVariable("id") Long id) {
+      Producto producto = productoRepository.findById(id)
+         .orElseThrow(() -> new EntityNotFoundException("Producto no encontrado"));
+      model.addAttribute("producto", producto);
+      return "editar";
+   }
+
+   @PostMapping("/actualizar")
+   String actualizarProducto(Model model, Producto producto) {
       productoRepository.save(producto);
       return "redirect:/productos";
    }
