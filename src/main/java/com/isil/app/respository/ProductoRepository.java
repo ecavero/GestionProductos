63359f3d7cbef15eeb3179aba8058552dc8d1f96
Eduardo Cavero <ecavero@gmail.com>
2025-10-29 15:04:36 -0500

    Corregir el ProductoRepository. Debe ser Long y no Integer.

--- src/main/java/com/isil/app/respository/ProductoRepository.java
+++ src/main/java/com/isil/app/respository/ProductoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
 }
