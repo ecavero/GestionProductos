1e9cb10cd72b57328639907a468ad79fd7633892
Eduardo Cavero <ecavero@gmail.com>
2025-10-29 14:20:05 -0500

    Modificando las rutas y agregando el método para guardar el nuevo producto.

--- src/main/java/com/isil/app/controller/ProductoController.java
+++ src/main/java/com/isil/app/controller/ProductoController.java
@@ -7,26 +7,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/productos")
 public class ProductoController {
 
    @Autowired
    ProductoRepository productoRepository;
 
-   @GetMapping("/productos")
+   @GetMapping
    String listarProductos(Model model) {
      List<Producto> productos = productoRepository.findAll();
      model.addAttribute("productos", productos);
      return "index";
    }
 
-   @GetMapping("/productos/nuevo")
+   @GetMapping("/nuevo")
    String nuevoProducto(Model model) {
       Producto producto = new Producto();
       model.addAttribute("producto", producto);
       return "nuevo";
+   }
+
+   @PostMapping
+   String nuevoProducto(Model model, Producto producto) {
+      productoRepository.save(producto);
+      return "redirect:/productos";
    }
 }
