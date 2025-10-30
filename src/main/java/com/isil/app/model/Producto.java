98f37bc5a22a11d6bdb4cd61ca2e3d1180b61a2f
Jhairolopez23 <jhonatanlopezr0203@gmail.com>
2025-10-30 05:12:25 -0500

    Se agregó anotaciones para las validaciones.

--- src/main/java/com/isil/app/model/Producto.java
+++ src/main/java/com/isil/app/model/Producto.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Entity
@@ -16,17 +17,23 @@ public class Producto {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
+   @NotBlank(message = "El código es obligatorio")
    @Column(unique = true, nullable = false)
    private String codigo;
 
    @Column(nullable = false)
+   @NotBlank(message = "El nombre es obligatorio")
    private String nombre;
 
    private String descripcion;
 
    @Column(nullable = false)
+   @NotNull(message = "El precio es obligatorio")
+   @DecimalMin(value = "0.0", inclusive = true, message = "El precio debe ser mayor o igual a 0")
    private Double precio;
 
+   @NotNull(message = "El stock es obligatorio")
+   @Min(value = 0, message = "El stock no puede ser negativo")
    @Column(nullable = false)
    private Integer stock;
 }
