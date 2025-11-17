a9d1a27917c98fa2374d26d4bcd423fc3c716dbf
Jhairolopez23 <jhonatanlopezr0203@gmail.com>
2025-11-17 16:59:26 -0500

    @Bean para la paginación

--- src/main/java/com/isil/app/ProductosApplication.java
+++ src/main/java/com/isil/app/ProductosApplication.java
@@ -2,6 +2,8 @@ package com.isil.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class ProductosApplication {
@@ -9,5 +11,10 @@ public class ProductosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductosApplication.class, args);
 	}
+
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 
 }
