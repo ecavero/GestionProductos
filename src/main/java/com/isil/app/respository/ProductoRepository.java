72e1b2e048db84e5ddb90837101987dc937db6de
Eduardo Cavero <ecavero@gmail.com>
2025-11-17 12:34:57 -0500

    Agregando método para buscar por nombre y paginar.

--- src/main/java/com/isil/app/respository/ProductoRepository.java
+++ src/main/java/com/isil/app/respository/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.isil.app.respository;
 
 import com.isil.app.model.Producto;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
+   Page<Producto> findByNombreContaining(String nombre, Pageable pageable);
 }
